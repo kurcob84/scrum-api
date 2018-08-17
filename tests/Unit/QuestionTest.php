04f3b002255f31e7372bb72dc73574c6edd14b01
Patrick Rogge <roggepatrick@googlemail.com>
2018-08-17 06:05:41 +0200

    test

---
+++ tests/Unit/QuestionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class QuestionTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function questionReadTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
